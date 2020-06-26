ffa12eee61a646998f9d5f5e0a15972a25c3312f
gkwlsdn17 <gkwlsdn17@gmail.com>
2020-06-26 16:37:47 +0900

    0626_modify

---
+++ Todayworkwork/오늘부터 1일/Login/Login/Individual/IMemberMainForm.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Login
+{
+    public partial class IMemberMainForm : Form
+    {
+        private static bool formOpen = false;
+
+        //마우스로 창 이동
+        bool On;
+        Point Pos;
+
+        private Button currentButton = null;
+        //DB연결
+        string strconn = "Data Source=munggu.iptime.org,11113;Initial Catalog=TodayWorkWork;Persist Security Info=True;User ID=sa;Password=8765432!";
+        private static string userid;
+        private static Customer user = new Customer();
+        public IMemberMainForm()
+        {
+            InitializeComponent();
+            Timer timer = new Timer();
+            timer.Tick += new EventHandler(timer1_Tick);
+            timer.Start();
+
+        }
+        private void readUserInfo(string id) //로그인 된 사용자의 정보를 읽어서 Com_customer클래스에 넣어줌
+        {
+            SqlConnection sqlcon = new SqlConnection(strconn);
+            sqlcon.Open();
+            string table_name = "CUSTOMER";
+            SqlCommand cmd = new SqlCommand($"select * from {table_name} where ID = @id", sqlcon);
+            cmd.Parameters.AddWithValue("@id", id);
+            SqlDataReader sdr = cmd.ExecuteReader();
+
+            while (sdr.Read())
+            {
+                user.ID = sdr["ID"].ToString();
+                user.PW = sdr["PW"].ToString();
+                lb_username.Text = sdr["NAME"].ToString();
+                user.NAME = lb_username.Text;
+                user.ADDR = sdr["ADDR"].ToString();
+                user.PHONE = sdr["PHONE"].ToString();
+                user.EMAIL = sdr["EMAIL"].ToString();
+            }
+            sqlcon.Close();
+        }
+        /*
+        private void readName(string id)
+        {
+            SqlConnection sqlcon = new SqlConnection(strconn);
+            sqlcon.Open();
+            SqlCommand cmd = new SqlCommand("select * from CUSTOMER where id = @id", sqlcon);
+            cmd.Parameters.AddWithValue("@id", id);
+            SqlDataReader sdr = cmd.ExecuteReader();
+
+            while (sdr.Read())
+            {
+                lb_username.Text = sdr["NAME"].ToString();
+            }
+            sqlcon.Close();
+        }
+        */
+        private void ActivateButton(object btnSender)
+        {
+            if (btnSender != null)
+            {
+                if (currentButton != null)
+                {
+                    currentButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(70)))), ((int)(((byte)(166)))));
+                    currentButton.ForeColor = System.Drawing.Color.White;
+
+
+                }
+                if (currentButton != (Button)btnSender)
+                {
+                    currentButton = (Button)btnSender;
+
+                    currentButton.BackColor = System.Drawing.Color.White;
+                    currentButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(70)))), ((int)(((byte)(166)))));
+
+                }
+            }
+        }
+        private void btn_menu1_Click(object sender, EventArgs e)
+        {
+            iMenu11.BringToFront();
+            ActivateButton(btn_menu1);
+        }
+
+        private void btn_menu2_Click(object sender, EventArgs e)
+        {
+            iMenu21.BringToFront();
+            ActivateButton(btn_menu2);
+        }
+
+        private void btn_menu3_Click(object sender, EventArgs e)
+        {
+            iMenu31.BringToFront();
+            ActivateButton(btn_menu3);
+        }
+
+        private void btn_menu4_Click(object sender, EventArgs e)
+        {
+            iMenu41.BringToFront();
+            ActivateButton(btn_menu4);
+        }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            On = true;
+            Pos.X = e.X;
+            Pos.Y = e.Y;
+        }
+
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (On == true)
+            {
+                this.SetDesktopLocation(MousePosition.X - Pos.X, MousePosition.Y - Pos.Y);
+            }
+        }
+
+        private void panel1_MouseUp(object sender, MouseEventArgs e)
+        {
+            On = false;
+        }
+
+        private void btn_exit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            DateTime time = DateTime.Now;
+            string timeView = time.ToString("yyyy") + "년 " + time.ToString("MM") + "월 " + time.ToString("dd") + "일 " + "(" + time.ToString("ddd") + ") " + time.ToString("hh") + "시" + time.ToString("mm") + "분" + time.ToString("ss") + "초";
+            lb_date.Text = timeView;
+        }
+
+        private void IMemberMainForm_Load(object sender, EventArgs e)
+        {
+            if (formOpen == false)
+            {
+                this.Close();
+            }
+            //사용자 이름을 불러옴
+            readUserInfo(userid);
+        }
+        public static void setID(string id)
+        {
+            userid = id;
+        }
+        public static void setFormOpen(bool open)
+        {
+            formOpen = open;
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            afterLoginMainPage1.BringToFront();
+        }
+    }
+}
---
+++ Todayworkwork/오늘부터 1일/Login/Login/LogIn/PWFind.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Login
+{
+    public partial class PWFind : Form
+    {
+        private string strconn = "Data Source=munggu.iptime.org,11113;Initial Catalog=TodayWorkWork;Persist Security Info=True;User ID=sa;Password=8765432!";
+        private string table_name = "";
+
+        public PWFind()
+        {
+            InitializeComponent();
+            rbtn_customer.Checked = true;
+        }
+        // 찾기 버튼 클릭
+        private void btn_pwFind_Click(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection(strconn);
+            SqlCommand cmd;
+            SqlDataAdapter sda;
+            DataSet ds = new DataSet();
+            conn.Open();
+            if (rbtn_customer.Checked == true)
+            {
+                table_name = "CUSTOMER";
+                cmd = new SqlCommand($"select PW from {table_name} where NAME = @NAME and ID = @ID and PHONE = @PHONE and EMAIL = @EMAIL", conn);
+                cmd.Parameters.AddWithValue("@NAME", text_name.Text);
+                cmd.Parameters.AddWithValue("@ID", text_id.Text);
+                cmd.Parameters.AddWithValue("@PHONE", text_tel.Text);
+                cmd.Parameters.AddWithValue("@EMAIL", text_email.Text);
+                sda = new SqlDataAdapter(cmd);
+                sda.Fill(ds);
+                // db 정보 있을 경우
+                if (ds.Tables[0].Rows.Count == 1)
+                {
+                    DataRow dr;
+                    dr = ds.Tables[0].Rows[0];
+                    string temp = (string)dr[0];
+                    MessageBox.Show("비밀번호 : " + temp);
+                }
+                // db에 정보가 없을 경우
+                else
+                {
+                    MessageBox.Show("등록된 회원 정보가 없습니다.");
+                }
+                conn.Close();
+            }
+            else
+            {
+                table_name = "COM_CUSTOMER";
+                cmd = new SqlCommand($"select PW from {table_name} where NAME = @NAME and ID = @ID and PHONE = @PHONE and EMAIL = @EMAIL", conn);
+                cmd.Parameters.AddWithValue("@NAME", text_name.Text);
+                cmd.Parameters.AddWithValue("@ID", text_id.Text);
+                cmd.Parameters.AddWithValue("@PHONE", text_tel.Text);
+                cmd.Parameters.AddWithValue("@EMAIL", text_email.Text);
+                sda = new SqlDataAdapter(cmd);
+                sda.Fill(ds);
+                // db 정보 있을 경우
+                if (ds.Tables[0].Rows.Count == 1)
+                {
+                    DataRow dr;
+                    dr = ds.Tables[0].Rows[0];
+                    string temp = (string)dr[0];
+                    MessageBox.Show("비밀번호 : " + temp);
+                }
+                // db에 정보가 없을 경우
+                else
+                {
+                    MessageBox.Show("등록된 회원 정보가 없습니다.");
+                }
+                conn.Close();
+            }
+        }
+    }
+}
