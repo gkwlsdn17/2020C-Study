f4b7c5c889c3769cc8441c85b11e1bf36112dcd0
gkwlsdn17 <gkwlsdn17@gmail.com>
2020-07-06 22:46:57 +0900

    0706_22_46_modify

---
+++ Todayworkwork/오늘부터 1일/Login/Login/Company/ChangeComMyInfo.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Login.Company
+{
+    public partial class ChangeComMyInfo : Form
+    {
+        //마우스로 창 이동
+        bool On;
+        Point Pos;
+
+        //메인폼에 메시지 전달
+        public delegate void sendMsgDelegate(string msg);
+        public event sendMsgDelegate sendMsg;
+
+        public ChangeComMyInfo()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_exit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void ChangeMyInfo_Load(object sender, EventArgs e)
+        {
+            FormBorderStyle = FormBorderStyle.None;
+            text_phone.Text = MainForm.getPhone();
+            text_name.Text = MainForm.getName();
+            text_addr.Text = MainForm.getAddr();
+            text_email.Text = MainForm.getEmail();
+            text_comName.Text = MainForm.getCom_name();
+            text_comTel.Text = MainForm.getCom_tel();
+            text_comAddr.Text = MainForm.getCom_addr();
+            text_comNum.Text = MainForm.getCom_num();
+        }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            On = true;
+            Pos.X = e.X;
+            Pos.Y = e.Y;
+        }
+
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (On == true)
+            {
+                this.SetDesktopLocation(MousePosition.X - Pos.X, MousePosition.Y - Pos.Y);
+            }
+        }
+
+        private void panel1_MouseUp(object sender, MouseEventArgs e)
+        {
+            On = false;
+        }
+
+        // 이름, 주소, 회사이름, 회사주소, 사업자번호, 회원 핸드폰번호, 회사 전화번호, 회원 이메일 정보를 업데이트 한다.
+        private void btn_update_Click(object sender, EventArgs e)
+        {
+            SqlConnection sqlcon = new SqlConnection(DBConnection.strconn);
+            try
+            {
+                sqlcon.Open();
+                SqlCommand cmd = new SqlCommand("update COM_CUSTOMER set NAME=@name, ADDR=@addr, COM_NAME=@com_name"+
+                    ", COM_ADDR=@com_addr, COM_NUM=@com_num, PHONE = @phone"+
+                    ", COM_TEL=@com_tel, EMAIL=@email where ID = @id",sqlcon);
+                cmd.Parameters.AddWithValue("@name", text_name.Text);
+                cmd.Parameters.AddWithValue("@addr", text_addr.Text);
+                cmd.Parameters.AddWithValue("@com_name", text_comName.Text);
+                cmd.Parameters.AddWithValue("@com_addr", text_comAddr.Text);
+                cmd.Parameters.AddWithValue("@com_num", text_comNum.Text);
+                cmd.Parameters.AddWithValue("@phone", text_phone.Text);
+                cmd.Parameters.AddWithValue("@com_tel", text_comTel.Text);
+                cmd.Parameters.AddWithValue("@email", text_email.Text);
+                cmd.Parameters.AddWithValue("@id", MainForm.getID());
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("수정이 완료되었습니다!");
+                sendMsg("OK");
+            }
+            catch(Exception ee)
+            {
+                Console.WriteLine(ee.StackTrace);
+            }
+            finally
+            {
+                if(sqlcon!=null)
+                    sqlcon.Close();
+                this.Close();
+            }
+            
+                      
+        }
+    }
+}
---
+++ Todayworkwork/오늘부터 1일/Login/Login/Individual/Resume_menu3/IMenu3.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using Login.Individual.Resume_menu3;
+using LogIn;
+
+namespace Login
+{
+    public partial class IMenu3 : UserControl
+    {
+        //DB연결
+        string strconn = DBConnection.strconn;
+        //Customer 클래스
+        static Customer user = Form1.getUserInstance();
+        //이력서insert
+        static InsertResume Ir = new InsertResume();
+        //이력서 정보 가져오기
+        static ResumeInfo Ri = new ResumeInfo();
+        //이력서 번호
+        int count = 0;
+
+        public IMenu3()
+        {
+            InitializeComponent();
+        }
+        private void CountReNum()
+        {
+            SqlConnection sqlconn = new SqlConnection(strconn);
+            sqlconn.Open();
+            SqlCommand rcmd = new SqlCommand("select * from RESUME where RE_NUM = (select MAX(RE_NUM) from RESUME)", sqlconn);
+            SqlDataReader sdr = rcmd.ExecuteReader();
+            sdr.Read();
+            int.TryParse(sdr["RE_NUM"].ToString(), out count);
+            count++;
+            sqlconn.Close();
+        }
+        //이력서 insert sql
+        private void Resume_Insert()
+        {
+            CountReNum();
+            SqlConnection sqlconn = new SqlConnection(strconn);
+            try
+            {
+                sqlconn.Open();
+                string InsertText = "Insert Into RESUME (RE_NUM,ID,NAME,ADDR,EMAIL,PHONE,RE_SUBJECT,LICENSE,EXP,CONTENT,LOCATION) VALUES(@re_num,@id,@name,@addr,@email,@phone,@re_subject,@license,@exp,@content,@location) ON DUPLICATE ON KEY UPDATE id = VALUES(@id)";
+                SqlCommand cmd = new SqlCommand(InsertText, sqlconn);
+
+                cmd.Parameters.AddWithValue("@re_num", count);
+                cmd.Parameters.AddWithValue("@id", IMemberMainForm.getID());
+                cmd.Parameters.AddWithValue("@name", IMemberMainForm.getName());
+                cmd.Parameters.AddWithValue("@addr", IMemberMainForm.getAddr());
+                cmd.Parameters.AddWithValue("@email", IMemberMainForm.getEmail());
+                cmd.Parameters.AddWithValue("@phone", IMemberMainForm.getPhone());
+                cmd.Parameters.AddWithValue("@re_subject", IMenu3.getSbj());
+                cmd.Parameters.AddWithValue("@license", IMenu3.getLic());
+                cmd.Parameters.AddWithValue("@exp", IMenu3.getEx());
+                cmd.Parameters.AddWithValue("@content", IMenu3.getCon());
+                cmd.Parameters.AddWithValue("@location", IMenu3.getLoca());
+
+                cmd.ExecuteNonQuery();
+                sqlconn.Close();
+            }
+            catch (Exception ee)
+            {
+                MessageBox.Show(ee.Message);
+                MessageBox.Show(ee.StackTrace);
+            }
+            finally
+            {
+                sqlconn.Close();
+            }
+        }
+        //이력서 수정
+        private void Resume_Update()
+        {
+            SqlConnection sqlconn = new SqlConnection(strconn);
+            try
+            {
+                sqlconn.Open();
+                string UpdateText = "update RESUME set RE_SUBJECT = @re_subject, LICENSE=@license, EXP =@exp, CONTENT =@content, LOCATION =@location where ID = @id";
+
+                SqlCommand cmd = new SqlCommand(UpdateText, sqlconn);
+                cmd.Parameters.AddWithValue("@id", IMemberMainForm.getID());
+                cmd.Parameters.AddWithValue("@re_subject", IMenu3.getSbj());
+                cmd.Parameters.AddWithValue("@license", IMenu3.getLic());
+                cmd.Parameters.AddWithValue("@exp", IMenu3.getEx());
+                cmd.Parameters.AddWithValue("@content", IMenu3.getCon());
+                cmd.Parameters.AddWithValue("@location", IMenu3.getLoca());
+
+                cmd.BeginExecuteNonQuery();
+                sqlconn.Close();
+            }catch(Exception ee)
+            {
+                MessageBox.Show(ee.Message);
+                MessageBox.Show(ee.StackTrace);
+            }
+            finally
+            {
+                sqlconn.Close();
+            }
+        }
+
+        private void Resume_Name_Click(object sender, EventArgs e) { }
+        //iMenu폼에 로그인시 바로 개인정보 불러오기
+        private void IMenu3_Load(object sender, EventArgs e)
+        {
+            /*
+            Resume_Name.Text = user.NAME;
+            Resume_Phone.Text = user.PHONE;
+            Resume_Addr.Text = user.ADDR;
+            Resume_Email.Text = user.EMAIL;
+            Subject_text.Text = ResumeInfo.getSubject();
+            Location_text.Text = ResumeInfo.getLocation();
+            Exp_text.Text = ResumeInfo.getExp();
+            License_text.Text = ResumeInfo.getLicense();
+            Introduce_text.Text = ResumeInfo.getContent();
+            */
+            setting();
+        }
+        
+        public void setting()
+        {
+            Resume_Name.Text = user.NAME;
+            Resume_Phone.Text = user.PHONE;
+            Resume_Addr.Text = user.ADDR;
+            Resume_Email.Text = user.EMAIL;
+            ResumeInfo info = new ResumeInfo();
+            info.readRESUMEInfo(user.ID);
+            Subject_text.Text = ResumeInfo.getSubject();
+            Location_text.Text = ResumeInfo.getLocation();
+            Exp_text.Text = ResumeInfo.getExp();
+            License_text.Text = ResumeInfo.getLicense();
+            Introduce_text.Text = ResumeInfo.getContent();
+        }
+        private void Resume_Location_Click(object sender, EventArgs e) { }
+
+        //찾기버튼과 선택폼 연동(희망지역)
+        private void btn_Location_Click(object sender, EventArgs e)
+        {
+            Resume_LocationForm Rlf = new Resume_LocationForm("");
+            Rlf.onSubFormSendEvent += setTextBox;
+            Rlf.ShowDialog();
+
+        }
+        //연동된 데이터값 확인(희망지역)
+        public void setTextBox(string loc)
+        {
+            if (loc == "")
+            {
+                Location_text.Text = "전달 받은 Data가 없습니다!";
+            }
+            else
+            {
+                Location_text.Text = loc;
+            }
+        }
+        //자격증 연동
+        private void btn_License_Click(object sender, EventArgs e)
+        {
+            Resume_License Rlc = new Resume_License("");
+            Rlc.onSubFormSendEvent += setTextBox1;
+            Rlc.ShowDialog();
+        }
+        //연동된 데이터값 확인(자격증)
+        public void setTextBox1(string lic)
+        {
+            if (lic == "")
+            {
+                License_text.Text = "전달 받은 Data가 없습니다!";
+            }
+            else
+            {
+                License_text.Text = lic;
+            }
+        }
+
+        //데이터값 저장
+        public static string getSbj()
+        {
+            return Ir.resume_Subject;
+        }
+        public static string getLoca()
+        {
+            return Ir.resume_Location;
+        }
+        public static string getEx()
+        {
+            return Ir.resume_Exp;
+        }
+        public static string getLic()
+        {
+            return Ir.resume_License;
+        }
+        public static string getCon()
+        {
+            return Ir.resume_Content;
+        }
+
+        private void Insert_Resume_Click(object sender, EventArgs e)
+        {
+            Ir.resume_Subject = Subject_text.Text;
+            Ir.resume_Location = Location_text.Text;
+            Ir.resume_Exp = Exp_text.Text;
+            Ir.resume_License = License_text.Text;
+            Ir.resume_Content = Introduce_text.Text;
+
+            Resume_Insert();
+            MessageBox.Show("등록되었습니다.");
+        }
+
+        private void Update_Resume_Click(object sender, EventArgs e)
+        {
+            Ir.resume_Subject = Subject_text.Text;
+            Ir.resume_Location = Location_text.Text;
+            Ir.resume_Exp = Exp_text.Text;
+            Ir.resume_License = License_text.Text;
+            Ir.resume_Content = Introduce_text.Text;
+
+            Resume_Update();
+            MessageBox.Show("수정되었습니다.");
+        }
+    }
+}
